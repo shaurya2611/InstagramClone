8ce2c369b22b89da59ee065db300b2349a17979a
Shaurya Singh <shaurya2611@gmail.com>
2024-06-04 12:14:53 +0530

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Shaurya Singh on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
