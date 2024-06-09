21b6e1b3bf256f0f31077a14b8ca712fed784386
Shaurya Singh <shaurya2611@gmail.com>
2024-06-09 16:20:13 +0530

    added firebase

--- InstagramClone/Core/UploadPost/UploadPostViewModel.swift
+++ InstagramClone/Core/UploadPost/UploadPostViewModel.swift
@@ -6,5 +6,20 @@
 //
 
 import Foundation
+import SwiftUI
+import PhotosUI
 
-
+@MainActor
+class UploadPostViewModel: ObservableObject{
+    
+    @Published var selectedImage: PhotosPickerItem?{
+        didSet{
+            Task { await loadImage(formItem: selectedImage)}
+        }
+    }
+    
+    func loadImage(formItem item: PhotosPickerItem?) async{
+        
+    }
+    
+}
