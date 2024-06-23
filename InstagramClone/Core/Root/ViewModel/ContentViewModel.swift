78891db2ba5f23dd307d7e8859d200776db8abc7
Shaurya Singh <shaurya2611@gmail.com>
2024-06-24 01:42:32 +0530

    conteniview model update

--- InstagramClone/Core/Root/ViewModel/ContentViewModel.swift
+++ InstagramClone/Core/Root/ViewModel/ContentViewModel.swift
@@ -5,25 +5,26 @@
 //  Created by Shaurya Singh on 12/06/24.
 //
 
-import Foundation
-import FirebaseAuth
 import Combine
+import FirebaseAuth
 
-class ContentViewModel: ObservableObject{
+class ContentViewModel: ObservableObject {
     
     private let service = AuthService.shared
     private var cancellables = Set<AnyCancellable>()
     
     @Published var userSession: FirebaseAuth.User? = nil
     
-    init(){
+    init() {
         setupSubscriber()
     }
     
-    func setupSubscriber(){
-        service.$userSession.sink { [weak self] userSession in
-            self?.userSession = userSession
-        }.store(in: &cancellables)
+    func setupSubscriber() {
+        service.$userSession
+            .receive(on: DispatchQueue.main) // Ensure updates are on the main thread
+            .sink { [weak self] userSession in
+                self?.userSession = userSession
+            }
+            .store(in: &cancellables)
     }
-    
 }
